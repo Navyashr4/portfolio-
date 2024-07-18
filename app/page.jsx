09ee3bd74b096e8e50bcd8c34a7c561b447fe268
Navya Shree <navyashr4@gmail.com>
2024-07-18 11:02:51 +0530

    initial files work

---
+++ app/page.jsx
@@ -0,0 +1,15 @@
+import {Button} from "@/components/ui/button"
+import {fiDownload} from "react-icons/fi"
+
+
+const Home = () => {
+  return (
+    <section>
+      <div>
+        Homepage 
+      </div>
+    </section>
+  )
+}
+
+export default Home
